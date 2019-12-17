6f9448f55520871eadaaec96f70e7ef93ceb0d02
nicbongo <nicbongo@protonmail.com>
2019-12-16 20:34:26 -0400

    Movies MVP

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from "react-router-dom";
-// import MovieCard from "./MovieCard";
+import MovieCard from "./MovieCard";
 import axios from 'axios';
 
 const MovieList = props => {
